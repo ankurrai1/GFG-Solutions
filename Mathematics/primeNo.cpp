5cbfc7f4aa072e1f92927d122e656e3c67c774d7
Ankur Rai <rai.ankur0007@gmail.com>
2021-02-07 01:00:01 +0530

    Problem No :        6 | function to find  given number is pirme or not in Maths Section

---
+++ Mathematics/primeNo.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// this will hold linear worst time complexity
+
+bool PrimeNaive(long num){
+    for(int i = 2; i < num; i++){
+        if(num % i == 0) return false;
+    }
+    return true;
+}
+
+// time complexity will be O(n^1/2);
+
+bool improvedPrime(long num){
+
+    for(int i = 2; i * i < num; i++){
+        if(num % i == 0) return false;
+    }
+    return true;
+
+}
+
+// 3 times faster then improvedPrime version
+
+bool fastPrime(long num){
+    if(num <= 1 || num % 2 == 0 || num % 3 == 0) return false;
+    if(num == 2 || num == 3) return true;
+    for(int i = 5; i * i < num; i+=6){
+        if(num % i == 0 || num % (i+2) == 0) return false;
+    }
+    return true;
+
+
+}
+
+int main(){
+    long num;
+    cin >> num;
+    cout << PrimeNaive(num) << "   " << improvedPrime(num) << "   " << fastPrime(num) <<endl;
+    return 0;
+}
