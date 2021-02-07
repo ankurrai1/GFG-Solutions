b8821408f6ba2c49dfe815d4a2512853d5faf272
Ankur Rai <rai.ankur0007@gmail.com>
2021-02-07 21:33:26 +0530

    Problem No :        8 | prime factors problem solution version 2 in Maths Section

--- Mathematics/primeFactors.cpp
+++ Mathematics/primeFactors.cpp
@@ -1,3 +1,5 @@
+// finding lcm using two methods:
+
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -12,26 +14,63 @@ bool isPrime(long num){
     return true;
 }
 
-// naive implementation of getting prime factors
-
 void PrintPrimeFactorsNaive(long n){
-    long count = 2;
-    while(count < n){
-        if(isPrime(count)){
-            long i =  count;
-            while(n % i == 0){
-                cout << count << " ";
-                i *= count;
+    for(int i = 2; i < n; i++){
+        if(isPrime(i)){
+            int t =  i;
+            while(n % t == 0){
+                cout << i << " ";
+                t *= i;
             }
         }
-        count++;
     }
 
+}
+
+void PrintPrimeFactors(long n){
+  if(n <= 1) return;
+  for(int i = 2; i*i <= n; i++){
+      while(n % i == 0){
+          cout << i << " ";
+          n /= i;
+      }
+  }
+  if(n > 1) cout << n << " ";
+}
+
+// a more faster way to print prime factors it is 3 times more faster to above same as
+// prime test
+
+void PrintPrimeFactorsOpt(long n){
+  if(n <= 1) return;
+  while(n % 2 == 0){
+      cout << 2 << " ";
+      n /= 2;
+  }
+  while(n % 3 == 0){
+      cout << 3 << " ";
+      n /= 3;
+  }
+  for(int i = 3; i*i <= n; i+=6){
+      while(n % i == 0){
+          cout << i << " ";
+          n /= i;
+      }
+      while(n % (i + 2)== 0){
+          cout << (i+2) << " ";
+          n /= (i+2);
+      }
+  }
+  if(n > 3) cout << n << " ";
 }
 
 int main(){
     long num;
     cin >> num;
     PrintPrimeFactorsNaive(num);
+    cout << "\n-----------------------------------------\n";
+    PrintPrimeFactors(num);
+    cout << "\n-----------------------------------------\n";
+    PrintPrimeFactorsOpt(num);
     return 0;
 }
