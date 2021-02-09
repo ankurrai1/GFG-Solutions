3ec7943fb5961a5081b2dd8090850ac04c64394a
Ankur Rai <rai.ankur0007@gmail.com>
2021-02-10 00:01:19 +0530

    Problem No :       14 | pattern printing using math in Maths Section

---
+++ Mathematics/printPattern.cpp
@@ -0,0 +1,25 @@
+// pattern is like
+// 3 3 3 2 2 2 1 1 1
+// 3 3 2 2 1 1
+// 3 2 1
+
+
+
+void printPat(int n)
+{
+    for(int i = 0 ; i < n; i++){
+        int num = n, count = 1;
+        for(int j = 0; j < n * (n - i); j++){
+            if(j < (n-i) * count){
+                cout << num << " ";
+            }
+            else{
+                num--;
+                count++;
+                cout << num << " ";
+            }
+
+        }
+        cout <<"$";
+    }
+}
