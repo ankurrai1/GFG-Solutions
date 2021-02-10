834cc82d4ea3543f103a945bc32a1279d83522ec
Ankur Rai <rai.ankur0007@gmail.com>
2021-02-11 01:21:32 +0530

    Problem No :       16 | modulo inverse of given num under given mod number in Maths Section

--- Mathematics/InverseModulo.cpp
+++ Mathematics/InverseModulo.cpp
@@ -3,6 +3,7 @@
 // 1) Brut Force Approach
 // if Found then return modulo inverse;
 // else return -1;
+// time complexity will be O(m)
 
 int modInverse(int a, int m)
 {
@@ -10,4 +11,38 @@ int modInverse(int a, int m)
        if((a * i) % m == 1) return i; // formula = N * X === 1 (% m)
    }
    return -1;
+}
+
+// time complexity will be O(log m)
+// Fermat's little theorem
+
+
+// If we know m is prime, then we can also use Fermats’s little theorem to find the inverse.
+// a^m-1 ≡ 1 (mod m)
+// If we multiply both sides with a-1, we get
+// a^m-2 ≡ a^-1 (mod m)
+// a and m must be co-prime
+
+int gcd(int a, int b){
+    if(b == 0) return a;
+    return gcd(b , a % b);
+
+}
+
+int power(int a, int b, int m){
+  if(b == 0) return 1;
+  int temp = power(a, b/2, m) % m;
+  temp = (temp % m) * (temp % m);
+  if( b & 1) return (temp * a) % m;
+  else return temp;
+}
+
+int modInverse(int a, int m)
+{
+  if(gcd(a,m) != 1){
+     cout << "ERROR : NOT POSSIBLE TO FIND" << endl;
+     return;
+  }
+  return power(a, m - 2, m);
+
 }
