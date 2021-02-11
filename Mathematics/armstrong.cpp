b7e971c93667d8629db5d22478e2681fb294def2
Ankur Rai <rai.ankur0007@gmail.com>
2021-02-11 18:25:12 +0530

    Problem No :       18 | armstrong  problem in Maths Section

---
+++ Mathematics/armstrong.cpp
@@ -0,0 +1,14 @@
+// An Armstrong number of three digits is an integer such that the sum of the cubes of its digits is equal to the number itself
+
+
+string armstrongNumber(int n){
+       int temp = n;
+       int sum = 0;
+       while(temp > 0){
+           int digit = temp % 10;
+           sum += (digit * digit * digit);
+           temp /= 10;
+       }
+       if(sum == n) return "Yes";
+       else return "No";
+   }
