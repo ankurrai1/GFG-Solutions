21a0cd67dcdfd8d5fccc28c69f9c12e39d762d05
Ankur Rai <rai.ankur0007@gmail.com>
2021-02-13 01:55:42 +0530

    Problem No :       24 | find NcR for given no's improved solution Maths Section

--- Mathematics/NcR.cpp
+++ Mathematics/NcR.cpp
@@ -29,3 +29,24 @@ class Solution{
    }
 
 };
+
+// more optemized solution in the form of space
+
+class Solution{
+public:
+    int nCr(int n, int r){
+        int mod = 1000000007;
+        vector<int > C(r+1,0);
+
+        C[0] = 1;
+    	for (int i = 1; i <= n; i++) {
+        // the main positive point to note it is filling form right to left that is why we
+        // have previous row value
+    		for (int j = min(i, r); j > 0; j--){
+                // nCj = (n-1)Cj + (n-1)C(j-1);
+    			C[j] = (C[j] + C[j-1])%mod;
+    		}
+    	}
+    	return C[r]%mod;
+    }
+};
