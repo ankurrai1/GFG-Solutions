41aa0546d9f792c1c9bfac471df33fa20f141bdb
Ankur Rai <rai.ankur0007@gmail.com>
2021-02-15 00:08:30 +0530

    Problem No :       28 | Find Nth root of M solution in Maths Section

--- Mathematics/nth_root.cpp
+++ Mathematics/nth_root.cpp
@@ -24,3 +24,31 @@ class Solution{
 	    return -1;
 	}
 };
+
+
+
+// updated solution with time  complexity nlogm
+
+class Solution{
+	public:
+	int NthRoot(int n, int m)
+	{
+		long long int low = 1LL, high = m, mid;
+		long long int ans = -1;
+		while(low <= high){
+			mid = (low + high)/2;
+			long long int x = mid;
+			for(int i = 1; i < n; i++){
+				x *= mid;
+				if(x > m * 1LL)break;
+			}
+			if(x == m * 1LL){
+				ans = mid;
+				break;
+			}
+			else if(x > m)high = mid - 1;
+			else low = mid + 1;
+		}
+		return int(ans);
+	}
+};
