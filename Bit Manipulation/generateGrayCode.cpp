4a019d4dcdb809f37689fae98effea8658cbf89d
Ankur Rai <rai.ankur0007@gmail.com>
2021-03-07 04:00:30 +0530

    Problem No : 56 | Bit manipulation generate gray code upto n bits

---
+++ Bit Manipulation/generateGrayCode.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+vector <string> generateCode(int N)
+{
+    vector<string> res;
+    for(int i = 0; i < (1 << N); i++){
+        int gray = (i ^ (i >> 1));
+        bitset<32> r(gray);
+        string rStr  = r.to_string();
+        rStr = rStr.substr(32 -N);
+        res.push_back(rStr);
+    }
+    return res;
+}
