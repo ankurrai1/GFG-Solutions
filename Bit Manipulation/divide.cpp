1e925dd12aa564d39bc08e0c19f6f50e8ac2c731
Ankur Rai <rai.ankur0007@gmail.com>
2021-03-11 18:01:37 +0530

    Problem No :       54 | Bit manipulation updated devided without * / and %

--- Bit Manipulation/divide.cpp
+++ Bit Manipulation/divide.cpp
@@ -6,19 +6,42 @@ int devide(long long dividend, long long divisor){
     int sign = 1;
     if(divisor < 0 && dividend > 0) sign = -1;
     else if(divisor > 0 && dividend < 0) sign = -1;
-    
+
     long long q = 0, t = 0;
     dividend = abs(dividend);
     divisor = abs(divisor);
     cout << q << endl;
     for(int i = 31; i >= 0; i--){
-        if(t + (divisor << i) < dividend){ // (t + divisor * 2 ^ i)
+        if((t + divisor << i) < dividend){ // (t + divisor * 2 ^ i)
             t += divisor<<i;
             q |= ( 1LL << i); // with which 2 ^ i  we devided divisor * num = > num = 1<<i;
         }
     }
     return q * sign;
 }
+
+// find difference to which can cause ERROR
+    long long divide(long long dividend, long long divisor)
+    {
+        int
+        sign = (dividend < 0 ) ^ (divisor < 0) ?  // if any of the no is -ve then output is true for same false
+                 -1 : 1;
+
+        dividend = abs(dividend);
+        divisor = abs(divisor);
+
+        long long q = 0, t = 0;
+
+
+        for(int i = 31; i >= 0; i--){
+            if(t + (divisor << i) <= dividend ){
+                t += divisor << i;
+                q |= (1LL << i);
+            }
+        }
+
+        return q * sign;
+    }
 
 int main(){
     int a = 43, b = 8;
