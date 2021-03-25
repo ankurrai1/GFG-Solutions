891acea8d706e6709a34fa3fd72e8b80562e26e5
Ankur Rai <rai.ankur0007@gmail.com>
2021-03-25 21:46:55 +0530

    Problem No :       74 | Recursion Tower of hanoi puzzel for 3 towers and n disks UPDATED

--- Recursion/TowerOfHanoi.cpp
+++ Recursion/TowerOfHanoi.cpp
@@ -7,8 +7,8 @@ using namespace std;
 void TowerOfHanoi(int n,    char T1,  char T2,   char T3){
     if(n <= 0) return;
     TowerOfHanoi(n-1, T1, T3, T2); // 2 Disk = when n = 1 move disk 1 from T1 to T2 ==> this will recurs until n = 1
-    cout << " move disk " << n << " from " << T1 << " to " << T3 <<endl;
-    TowerOfHanoi(n-1, T2, T1, T3); 
+    cout << " move disk " << n << " from " << T1 << " to " << T3 <<endl; // this will be in order position of all  b/w 2 recursion
+    TowerOfHanoi(n-1, T2, T1, T3);
 }
 
 int main(){
