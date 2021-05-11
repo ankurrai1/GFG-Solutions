ca9429a7e130ede136499dff3e1b41f2c36d1c49
Ankur Rai <rai.ankur0007@gmail.com>
2021-05-11 20:16:19 +0530

    Problem No :       84| find element right paths and count them to reach end with k money in Recursion UPDATED

--- Recursion/noOfcoin.cpp
+++ Recursion/noOfcoin.cpp
@@ -22,7 +22,7 @@ public:
 
     long long countPath(int m, int n, int k, vector<vector<int>> arr){
         if(m < 0 || n < 0 || k < 0) return 0;
-        if(m == 0 && n == 0) return (k == arr[m][n]); // int value of this  will be 1;
+        if(m == 0 && n == 0) return (dp[m][n][k] = k == arr[m][n]); // int value of this  will be 1;
         if(dp[m][n][k] != -1) return dp[m][n][k];
 
         dp[m][n][k] = countPath(m - 1 , n, k - arr[m][n], arr)+
@@ -34,3 +34,44 @@ public:
         return countPath(n - 1, n - 1, k, arr);
     }
 };
+
+
+// gfg Solution given
+
+class Solution {
+public:
+    long long dp[101][101][101];
+    int a[101][101];
+
+    long long go(int n,int m,int k)
+    {
+        if(k<0)
+            return 0;
+        if(m<0||n<0)
+            return 0;
+        if(n==0&&m==0)
+            return dp[n][m][k]=(k==a[n][m]);
+        if(dp[n][m][k]!=-1)
+            return dp[n][m][k];
+
+        long long left = go(n,m-1,k-a[n][m]);
+        long long up = go(n-1,m,k-a[n][m]);
+        return dp[n][m][k] = left + up;
+    }
+
+    long long numberOfPath(int n, int k, vector<vector<int>> arr){
+
+        int i,j,l,m,t;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                a[i][j] = arr[i][j];
+            }
+        }
+        for(i=0;i<n;i++)
+                for(j=0;j<n;j++)
+                    for(l=0;l<=k;l++)
+                        dp[i][j][l]=-1;
+        go(n-1,n-1,k);
+        return dp[n-1][n-1][k];
+    }
+}
