76b958303dde9bdd095f30e4515549638e5662ab
Ankur Rai <rai.ankur0007@gmail.com>
2021-05-11 13:29:03 +0530

    Problem No :       82| updated Print permutation for Geeks for geeks in Recursion

--- Recursion/PrintPermutations.cpp
+++ Recursion/PrintPermutations.cpp
@@ -19,3 +19,28 @@ int main(){
   StrPermutation(str, n);
   return 0;
 }
+
+lass Solution
+{
+	public:
+	    void StrPermutation(string str, int n, int i, vector<string> &res){
+	        if(i == n){
+	            res.push_back(str);
+	            return;
+	        }
+	        for(int j = i; j < n; j++){
+	            swap(str[i], str[j]);
+	            StrPermutation(str, n , i+1, res);
+	            swap(str[i], str[j]);
+	        }
+}
+
+		vector<string>find_permutation(string S)
+		{
+		    vector<string> res;
+		    int n = S.size();
+		    StrPermutation(S, n, 0 , res);
+		    sort(res.begin(),res.end());
+		    return res;
+		}
+}
